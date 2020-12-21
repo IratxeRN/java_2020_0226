13ecc22d99e499df137c54c8d7af8a84b36ef869
JavierLete <javier@eskura21.com>
2020-12-21 12:29:17 +0100

    Cambiamos la configuración para usar el UsuarioDaoMySql

--- supermercado/src/com/ipartek/formacion/supermercado/controladores/Configuracion.java
+++ supermercado/src/com/ipartek/formacion/supermercado/controladores/Configuracion.java
@@ -3,10 +3,10 @@ package com.ipartek.formacion.supermercado.controladores;
 import com.ipartek.formacion.supermercado.accesodatos.Dao;
 import com.ipartek.formacion.supermercado.accesodatos.DaoUsuario;
 import com.ipartek.formacion.supermercado.accesodatos.ProductoDaoTreeMap;
-import com.ipartek.formacion.supermercado.accesodatos.UsuarioDaoTreeMap;
+import com.ipartek.formacion.supermercado.accesodatos.UsuarioDaoMySql;
 import com.ipartek.formacion.supermercado.modelos.Producto;
 
 public class Configuracion {
-	public static DaoUsuario daoUsuarios = UsuarioDaoTreeMap.getInstancia();
+	public static DaoUsuario daoUsuarios = UsuarioDaoMySql.getInstancia();
 	public static Dao<Producto> daoProductos = ProductoDaoTreeMap.getInstancia();
 }
