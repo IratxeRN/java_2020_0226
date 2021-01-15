8b54ce37578d8c55ee6d70e51d3c6aaf33fa17c7
JavierLete <javier@eskura21.com>
2021-01-15 11:03:15 +0100

    Recuperación de borrados

--- supermercado/sql/supermercado.sql
+++ supermercado/sql/supermercado.sql
@@ -477,6 +477,25 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `productos_recuperar` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `productos_recuperar`(p_id INT)
+BEGIN
+UPDATE productos SET activo = 1 WHERE id = p_id;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `sumar` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -599,4 +618,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-01-15 10:49:23
+-- Dump completed on 2021-01-15 11:02:49
--- supermercado/src/com/ipartek/formacion/supermercado/controladores/admin/ProductoBorrarServlet.java
+++ supermercado/src/com/ipartek/formacion/supermercado/controladores/admin/ProductoBorrarServlet.java
@@ -15,21 +15,29 @@ import com.ipartek.formacion.supermercado.modelos.Producto;
 @WebServlet("/admin/borrar")
 public class ProductoBorrarServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String id = request.getParameter("id");
-		
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String sId = request.getParameter("id");
+		String deshacer = request.getParameter("deshacer");
+		Long id = Long.parseLong(sId);
+
 		Dao<Producto> dao = Configuracion.daoProductos;
-		
-		dao.eliminar(Long.parseLong(id));
-		
+
+		if (deshacer != null) {
+			dao.recuperar(id);
+		} else {
+			dao.eliminar(id);
+		}
+
 		request.setAttribute("alertaTexto", "Borrado efectuado correctamente");
 		request.setAttribute("alertaNivel", "success");
-		
+
 		request.getRequestDispatcher("/admin/index").forward(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
